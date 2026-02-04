6549b39372805a1cbaf20d809ee174a45de5c737
NenadoPL1Z <limon4ikpro1@gmail.com>
2026-02-05 04:12:52 +0500

    feat: form

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,3 +1,62 @@
+import styles from "./form.module.css";
+import classnames from "classnames/bind";
+import { useState } from "react";
+
+const cn = classnames.bind(styles);
+
 export const Form = () => {
-  return <div>Form</div>;
+  const [name, setName] = useState("");
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.text}>
+        <h2 className={cn(styles.title, "sn-pro-regular")}>АНКЕТА</h2>
+        <p className={cn(styles.paragraph, "sn-pro-regular")}>
+          Будем очень признательны, если Вы сообщите нам о своем решении до
+          01.03.2026
+        </p>
+      </div>
+      <form className={styles.form}>
+        <div className={styles.formItem}>
+          <label
+            className={styles.label}
+            htmlFor="name-input">
+            Ваше имя и фамилия
+          </label>
+          <input
+            type="text"
+            id="name-input"
+            name="user_name"
+            max={50}
+            placeholder="ФИО"
+            className={styles.input}
+            value={name}
+            required
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className={styles.formItem}>
+          <label
+            className={styles.label}
+            htmlFor="radio">
+            Планируете ли вы присутствовать на свадьбе?
+          </label>
+          ...
+        </div>
+        <div className={styles.formItem}>
+          <label
+            className={styles.label}
+            htmlFor="checkbox">
+            Что предпочитаете из напитков?
+          </label>
+          ...
+        </div>
+        <button
+          type="submit"
+          className={styles.button}>
+          ОТПРАВИТЬ
+        </button>
+      </form>
+    </div>
+  );
 };
--- src/components/remaining-time/remaining-time.tsx
+++ src/components/remaining-time/remaining-time.tsx
@@ -1,3 +1,20 @@
+import styles from "./remaining-time.module.css";
+import classnames from "classnames/bind";
+
+const cn = classnames.bind(styles);
+
 export const RemainingTime = () => {
-  return <div>remaining-time</div>;
+  return (
+    <div className={styles.container}>
+      <h4 className={cn(styles.paragraph, "sn-pro-regular")}>
+        ДО ТОРЖЕСТВА ОСТАЛОСЬ:
+      </h4>
+      <div className={styles.remainingTime}>
+        <div className={styles.item}>0{"\n"}дней</div>
+        <div className={styles.item}>0{"\n"}часов</div>
+        <div className={styles.item}>0{"\n"}минут</div>
+        <div className={styles.item}>0{"\n"}секунд</div>
+      </div>
+    </div>
+  );
 };
