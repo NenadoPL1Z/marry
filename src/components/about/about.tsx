0be1c3bd2cb21f9d033ddab1670f49a6f3588e13
NenadoPL1Z <limon4ikpro1@gmail.com>
2026-02-06 01:05:35 +0500

    feat: about anim

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -3,8 +3,18 @@ import styles from "./about.module.css";
 export const About = () => {
   return (
     <div className={styles.container}>
+      <img
+        data-aos="slide-custom"
+        data-aos-duration="1000"
+        alt="m&v"
+        src="/public/images/maxim&valeria.jpg"
+        className={styles.image}
+      />
       <div className={styles.layer} />
-      <div className={styles.textWrapper}>
+      <div
+        data-aos="fade-left"
+        data-aos-duration="1000"
+        className={styles.textWrapper}>
         <span className={styles.small}>&</span>
         <h1 className={`${styles.name} great-vibes-regular`}>
           Maxim
